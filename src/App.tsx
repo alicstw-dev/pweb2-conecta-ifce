b443e1b74bb05b7319c3031a36b033707291bdb7
Alice <alicemariadelimamelo@gmail.com>
2026-03-20 19:31:52 -0300

    feat: adicionando página de Login com formulário, componentes de entrada e rótulo, e funcionalidade de exibição de senha

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import Footer from '@/components/shared/footer'
 import Navbar from '@/components/shared/navbar'
 import HomePage from '@/pages/HomePage'
+import LoginPage from '@/pages/LoginPage'
 
 function App() {
 	return (
 		<>
 			<div className="flex flex-col min-h-svh">
 				<Navbar />
-				<main className="flex-1">
-					<HomePage />
+				<main className="flex-1 flex flex-col">
+					{/* <HomePage /> */}
+					<LoginPage />
 				</main>
 				<Footer />
 			</div>
